49340ae466a94259946e15d923cb790539f26cc5
冷暖 <ysw209502@alibaba-inc.com>
2021-03-17 17:41:26 +0800

    Update

--- flaskdemo/demoapp.py
+++ flaskdemo/demoapp.py
@@ -9,10 +9,10 @@ from rds import redis_client, redis_client2, redis_client3
 
 
 
-@bp.route('/', methods=["POST"], strict_slashes=False)
+@bp.route('/', methods=["GET"], strict_slashes=False)
 def Indexf():
   '''
   默认视图，请求路径是   http://xxx/demo/ -> bluepoint.py中定义
   '''
   #Arg1 = request.args["arg1"]
-  return '200'
+  return render_template('index.html')
